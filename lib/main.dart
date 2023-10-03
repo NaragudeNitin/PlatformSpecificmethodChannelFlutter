fc1dced97b93609e815be554b8b570aac4e87a26
nitinVNCodewave <nitinnaragude@codewave.com>
2023-10-03 13:06:32 +0530

    Auto updating ui without any button press

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,26 @@ class _MyAppState extends State<MyApp> {
   static const platform = MethodChannel("samples.flutter.dev/battery");
 
   // Get battery level.
-  String _batteryLevel = 'Unknown battery level.';
+  final ValueNotifier<String> _batteryLevelNotifier =
+      ValueNotifier<String>('Unknown battery level.');
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start the periodic timer to update the battery level every 5 seconds.
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _getBatteryLevel();
+    });
+  }
+
+  @override
+  void dispose() {
+    _batteryLevelNotifier.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
 
   Future<void> _getBatteryLevel() async {
     String batteryLevel;
@@ -36,9 +55,7 @@ class _MyAppState extends State<MyApp> {
       batteryLevel = "Failed to get battery level: '${e.message}'.";
     }
 
-    setState(() {
-      _batteryLevel = batteryLevel;
-    });
+    _batteryLevelNotifier.value = batteryLevel;
   }
 
   @override
@@ -49,14 +66,11 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Center(
-                child: ElevatedButton(
-                  onPressed: _getBatteryLevel,
-                  child: const Text(
-                      textDirection: TextDirection.ltr, 'Get Battery Level'),
-                ),
-              ),
-              Text(_batteryLevel, textDirection: TextDirection.ltr),
+              ValueListenableBuilder<String>(
+                  valueListenable: _batteryLevelNotifier,
+                  builder: (context, batteryLevel, child) {
+                    return Text(batteryLevel, textDirection: TextDirection.ltr);
+                  }),
             ],
           ),
         ),
